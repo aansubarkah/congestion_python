d9f44b7b6335afc506459a4e568187211b18da74
aansubarkah <aansubarkah@gmail.com>
2017-06-04 10:09:42 +0700

    add classifying

--- congestion_processing.py
+++ congestion_processing.py
@@ -1,4 +1,6 @@
 from CongestionMining import main as mining
+from CongestionClassifying import main as classifying
 
 if __name__ == '__main__':
     mining()
+    classifying()
--- debe.py
+++ debe.py
@@ -50,3 +50,15 @@ class Respondent(BasePostgresTraffic):
     active = Column(Boolean, default=True)
     tmc = Column(Boolean, default=False)
 
+class Kind(BasePostgresTraffic):
+    __tablename__ = 'kinds'
+    id = Column(BigInteger, primary_key=True)
+    raw_id = Column(BigInteger())
+    classification_id = Column(Integer())
+    processed = Column(Boolean, default=False)
+    active = Column(Boolean, default=True)
+    created = Column(DateTime(), default=datetime.now)
+    modified = Column(DateTime(), default=datetime.now, onupdate=datetime.now)
+    t_id = Column(BigInteger())
+    verified = Column(Boolean, default=False)
+    trained = Column(Boolean, default=False)
